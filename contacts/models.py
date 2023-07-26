6267e8522412e2520fafbfd40e06cf1ccaa6c0f7
Gareth Macdonald <macdonald.gareth@gmail.com>
2023-07-26 11:50:55 +0200

    Add basic name fields to contact

--- contacts/models.py
+++ contacts/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+from core.models import TimeStampedModel
+
+
+class Contact(TimeStampedModel):
+    name = models.CharField(
+        _("name"),
+        blank=True,
+        null=False,
+        max_length=100,
+        help_text="Full name in the name order and language user would like. English or 日本語 for example. Max length: 100char",
+    )
+    name_en = models.CharField(
+        _("english name"),
+        blank=True,
+        null=False,
+        max_length=100,
+        help_text="Full name in English, order should be same as name. Example 田中たろ should become Tanaka Taro in the field. Max length: 100char",
+    )

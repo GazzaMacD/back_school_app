98dc4ca080544b0bd84e2c591762c30c9d670b5f
Gareth Macdonald <macdonald.gareth@gmail.com>
2023-07-29 17:29:51 +0200

    Basic contact url, view, serializer set up, new contact working

--- contacts/serializers.py
+++ contacts/serializers.py
@@ -1,9 +1,50 @@
 from rest_framework import serializers
 
-from .models import Contact
+from .models import Contact, ContactEmail, Note
 
 
 class ContactUserSerializer(serializers.ModelSerializer):
     class Meta:
         model = Contact
         fields = ("name",)
+
+
+# Contact Form Serializers
+
+
+class ContactFormContactEmailSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ContactEmail
+        fields = ("email",)
+
+
+class ContactFormNoteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Note
+        fields = ("note",)
+
+
+class ContactFormSerializer(serializers.ModelSerializer):
+    contact_emails = ContactFormContactEmailSerializer(many=True)
+    contact_notes = ContactFormNoteSerializer(many=True)
+
+    class Meta:
+        model = Contact
+        fields = ("name", "contact_emails", "contact_notes")
+
+    def create(self, validated_data):
+        email_data = validated_data.pop("contact_emails")
+        note_data = validated_data.pop("contact_notes")
+        contact = Contact.objects.create(**validated_data)
+        contact_email = None
+        for email in email_data:
+            email_dict = dict(email)
+            email_dict["is_primary"] = True
+            contact_email = email_dict["email"]
+            ContactEmail.objects.create(contact=contact, **email_dict)
+        for note in note_data:
+            note_dict = dict(note)
+            note_dict["note_type"] = 1
+            note_dict["title"] = f"Email from {contact_email}"
+            Note.objects.create(contact=contact, **note_dict)
+        return contact
--- contacts/views.py
+++ contacts/views.py
@@ -1,3 +1,45 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from rest_framework.permissions import AllowAny
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
-# Create your views here.
+from .models import Contact, ContactEmail
+from .serializers import ContactFormSerializer
+
+
+class ContactFormView(APIView):
+    """
+    Write or Update view for contact form
+
+    NOTE: Need IP Address permission class here
+    """
+
+    permission_classes = [AllowAny]
+
+    def get_object(self, email):
+        qs = ContactEmail.objects.filter(email__iexact=email)
+        if qs.exists():
+            contact_email = qs.first()
+            contact = contact_email.contact
+            return contact
+        else:
+            return None
+
+    def post(self, request, format=None):
+        email = request.data["contact_emails"][0]["email"]
+        email = self.get_object(email)
+        if not email:
+            # create new contact with new email and note
+            data = {"name": request.data["name"]}
+            serializer = ContactFormSerializer(data=request.data)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data)
+            return JsonResponse(
+                {"message": "ERROR"}, status=status.HTTP_400_BAD_REQUEST
+            )
+        else:
+            # update the relevant contact with new note
+            return JsonResponse({"message": "Updated"}, status=200)

d523f9a0ec40a10473377af34ac447888dde9f5b
Gareth Macdonald <macdonald.gareth@gmail.com>
2023-07-31 10:39:25 +0200

    More comprehensive model tests and intergration tests for api

--- contacts/tests/test_models.py
+++ contacts/tests/test_models.py
@@ -24,3 +24,24 @@ class ContactModelTests(TestCase):
         self.assertEqual(Contact.objects.count(), 0)
         user2 = User.objects.create_user(**USER1)
         self.assertEqual(Contact.objects.count(), 1)
+
+    def test_contact_is_related_to_user(self):
+        user = User.objects.create_user(**USER1)
+        contact = Contact.objects.first()
+        self.assertEqual(contact, user.contact)
+
+    def test_user_deletion_does_not_delete_contact(self):
+        user1 = User.objects.create_user(**USER1)
+        self.assertEqual(Contact.objects.count(), 1)
+        self.assertEqual(User.objects.count(), 1)
+        user1.delete()
+        self.assertEqual(User.objects.count(), 0)
+        self.assertEqual(Contact.objects.count(), 1)
+
+    def test_contact_email_is_created_new_user_creation(self):
+        self.assertEqual(ContactEmail.objects.count(), 0)
+        user2 = User.objects.create_user(**USER1)
+        contact = user2.contact
+        self.assertEqual(ContactEmail.objects.count(), 1)
+        email = ContactEmail.objects.first()
+        self.assertEqual(contact, email.contact)

67a41a38cfe76626a4c71b8a790d72a77d8a6843
Gareth Macdonald <macdonald.gareth@gmail.com>
2023-08-11 11:58:30 +0200

    Add telnumbers models to contact

--- telnumbers/models.py
+++ telnumbers/models.py
@@ -1,3 +1,46 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+from core.models import TimeStampedModel
+
+
+class TelNumberChoices(models.TextChoices):
+    HOME_FIXED = "HOF", _("Fixed Home Phone")
+    WORK_FIXED = "WOF", _("Fixed Work Phone")
+    PERSONAL_MOBILE = "PEM", _("Personal mobile")
+    WORK_MOBILE = "WOM", _("Work Mobile")
+
+
+class TelNumber(TimeStampedModel):
+    contact = models.ForeignKey(
+        "contacts.Contact",
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+    )
+    number = models.CharField(
+        _("Telephone Number"),
+        null=False,
+        blank=False,
+        max_length=15,
+        help_text=_("Required, max 15 char"),
+    )
+    number_type = models.CharField(
+        _("Type"),
+        null=False,
+        blank=False,
+        max_length=3,
+        choices=TelNumberChoices.choices,
+        default=TelNumberChoices.PERSONAL_MOBILE,
+        help_text=_("Required"),
+    )
+    is_primary = models.BooleanField(
+        _("Is primary"),
+        blank=False,
+        null=False,
+        default=False,
+        help_text=_("Required. Should be only one per place or contact"),
+    )
+
+    def __str__(self) -> str:
+        return self.number

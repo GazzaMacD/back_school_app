68265b83cbe983c08a9ad175682d28589fd7b676
Gareth Macdonald <macdonald.gareth@gmail.com>
2023-10-23 10:24:18 +0200

    Basic product model with custom slug or raise method, working as expected

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Product
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        "name",
+        "id",
+    )
+    readonly_fields = [
+        "slug",
+        "created",
+        "modified",
+    ]
+    fieldsets = [
+        (
+            "Product Basic Information",
+            {
+                "fields": [
+                    "name",
+                    "slug",
+                    "created",
+                    "modified",
+                ],
+            },
+        ),
+    ]
+
+
+admin.site.register(Product, ProductAdmin)

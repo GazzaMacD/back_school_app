588f845ca0a6a5e1ac5391e18ff325c4912db4f9
Gareth Macdonald <macdonald.gareth@gmail.com>
2024-03-24 05:26:27 +0100

    Change api max limit to 30 pages in base settings

--- config/settings/base.py
+++ config/settings/base.py
@@ -285,6 +285,7 @@ SIMPLE_JWT = {
 
 # Base URL to use when referring to full URLs within the Wagtail admin backend - # e.g. in notification emails. Don't include '/admin' or a trailing slash
 WAGTAILADMIN_BASE_URL = "http://example.com"
+WAGTAILAPI_LIMIT_MAX = 30
 
 # auto field for id settings
 DEFAULT_AUTO_FIELD = "django.db.models.AutoField"

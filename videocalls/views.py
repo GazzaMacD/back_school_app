dac3ed2599cc9995b36b46e7834a081f480bec0d
Gareth Macdonald <macdonald.gareth@gmail.com>
2025-05-19 14:20:27 +0200

    Lockdown api to authenticated only

--- videocalls/views.py
+++ videocalls/views.py
@@ -1,6 +1,6 @@
 from rest_framework.response import Response
 from rest_framework.views import APIView
-# from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated
 
 from .models import VideoCall
 from .serializers import VideoCallSerializer
@@ -10,7 +10,7 @@ class VideoCallListView(APIView):
     """Authenticated user only view for list view of all available teacher video calls"""
 
     # NOTE: Must remove comment here to lock down api
-    # permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated]
 
     def get(self, format=None):
         teachers = VideoCall.objects.all()
